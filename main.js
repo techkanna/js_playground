cdb8e416f3c2f8a796f0bf91291b3a3e2cec31cb
senthilkannan <senthil@techdew.com>
2023-02-09 13:49:24 +0530

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+ 
+
+// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const arr = []
+  let initialVal=[]
+  for(let i=0; i < nums.length; i++){
+     const num = nums[i]
+     if(num < target){
+         initialVal+=num
+         arr.push(i)
+     }
+     
+     if(initialVal=== target){
+      break
+     }
+  }  
+
+  return arr
+};
+
+console.log(JSON.stringify(twoSum([2,7,11,15], 9)) === JSON.stringify([0,1]));
+console.log(JSON.stringify(twoSum([3,2,4], 6)) === JSON.stringify([1,2]));
+console.log(JSON.stringify(twoSum([3,3], 6)) === JSON.stringify([0,1]));
+
